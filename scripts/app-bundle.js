23c3159cabf454d833265031f1256accdd1b4334
armikhalev <armikhalev@gmail.com>
2017-03-10 18:01:28 -0500

    Finishing Latay page

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -106,8 +106,37 @@ define('latay/latay',["require", "exports"], function (require, exports) {
     Object.defineProperty(exports, "__esModule", { value: true });
     var Latay = (function () {
         function Latay() {
-            this.message = 'Se ya Latay!';
+            this.cards = [
+                {
+                    front: "A",
+                    back: "Makes any word an infinitive verb",
+                    flip: false
+                },
+                {
+                    front: "E",
+                    back: "Mark of a direct object, accusative case",
+                    flip: false
+                },
+                {
+                    front: "I",
+                    back: "And",
+                    flip: false
+                },
+                {
+                    front: "O",
+                    back: "Honorification, high style, politeness.'Omaw - please'.",
+                    flip: false
+                },
+                {
+                    front: "U",
+                    back: "Some",
+                    flip: false
+                }
+            ];
         }
+        Latay.prototype.flipCard = function (_card) {
+            _card.flip = !_card.flip;
+        };
         return Latay;
     }());
     exports.Latay = Latay;
@@ -188,9 +217,10 @@ define('resources/attributes/on-enter',["require", "exports", "aurelia-framework
 });
 
 define('text!app.html', ['module'], function(module) { module.exports = "<template><require from=\"bootstrap/css/bootstrap.css\"></require><require from=\"./styles.css\"></require><require from=\"navig-bar/navig-bar\"></require><navig-bar router.bind=\"router\"></navig-bar><div class=\"container\"><router-view></router-view></div></template>"; });
-define('text!koyla/koyla.html', ['module'], function(module) { module.exports = "<template><require from=\"resources/attributes/on-enter\"></require><require from=\"./koyla.css\"></require><div id=\"koyla_page\" class=\"row text-center col-xs-offset-4 col-xs-4\"><button type=\"submit\" id=\"changeLanguageBtn\" class=\"btn btn-primary\" click.delegate=\"changeLanguage()\">Change language</button><form><div class=\"form-group\"><label id=\"languageTranslatedFromHeader\" class=\"formHeader\" for=\"wordToTranslate\">${lang}</label><input on-enter.call=\"submitWord()\" type=\"text\" placeholder=\"Type any word to translate\" id=\"wordToTranslate\" class=\"form-control wordToTranslate\" value.bind=\"word\"><br></div><button type=\"submit\" id=\"submitBtn\" class=\"btn btn-success\" click.delegate=\"submitWord()\">Submit</button></form><div class=\"row\"><label id=\"languageTranslatedToHeader\" class=\"formHeader\">${langToTranslate}</label><div id=\"translation\" class=\"translationDiv well well-lg\">${translation}</div></div></div></template>"; });
 define('text!styles.css', ['module'], function(module) { module.exports = "body {\n  padding-top: 65px; }\n\nh1 {\n  margin-top: 50%;\n  text-align: center; }\n"; });
+define('text!koyla/koyla.html', ['module'], function(module) { module.exports = "<template><require from=\"resources/attributes/on-enter\"></require><require from=\"./koyla.css\"></require><div id=\"koyla_page\" class=\"row text-center col-xs-offset-4 col-xs-4\"><button type=\"submit\" id=\"changeLanguageBtn\" class=\"btn btn-primary\" click.delegate=\"changeLanguage()\">Change language</button><form><div class=\"form-group\"><label id=\"languageTranslatedFromHeader\" class=\"formHeader\" for=\"wordToTranslate\">${lang}</label><input on-enter.call=\"submitWord()\" type=\"text\" placeholder=\"Type any word to translate\" id=\"wordToTranslate\" class=\"form-control wordToTranslate\" value.bind=\"word\"><br></div><button type=\"submit\" id=\"submitBtn\" class=\"btn btn-success\" click.delegate=\"submitWord()\">Submit</button></form><div class=\"row\"><label id=\"languageTranslatedToHeader\" class=\"formHeader\">${langToTranslate}</label><div id=\"translation\" class=\"translationDiv well well-lg\">${translation}</div></div></div></template>"; });
+define('text!latay/latay.html', ['module'], function(module) { module.exports = "<template><require from=\"./latay.css\"></require><div class=\"latayPage\"><div repeat.for=\"card of cards\" click.delegate=\"flipCard(card)\" class=\"flipContainer ${ card.flip === true ? 'flip' : ''}\"><div class=\"flipper\"><div class=\"card frontCard\">${card.front}</div><div class=\"card backCard\">${card.back}</div></div></div></div></template>"; });
 define('text!koyla/koyla.css', ['module'], function(module) { module.exports = ".formHeader {\n  margin-top: 24px;\n  color: tan;\n  font-size: 24px; }\n  .formHeader:first-letter {\n    text-transform: capitalize; }\n\n.translationDiv {\n  color: chocolate;\n  font-size: 26px; }\n"; });
-define('text!latay/latay.html', ['module'], function(module) { module.exports = "<template><h1>${message}</h1></template>"; });
 define('text!navig-bar/navig-bar.html', ['module'], function(module) { module.exports = "<template><nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\"><div class=\"navbar-header\"><button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\"><span class=\"sr-only\">Toggle Navigation</span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span></button> <a class=\"navbar-brand\" href=\"#\"><i class=\"fa fa-home\"></i> <span>${router.title}</span></a></div><div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\"><ul class=\"nav navbar-nav\"><li repeat.for=\"row of router.navigation\" class=\"${row.isActive ? 'active' : ''}\"><a data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1.in\" href.bind=\"row.href\">${row.title}</a></li></ul><ul class=\"nav navbar-nav navbar-right\"><li class=\"loader\" if.bind=\"router.isNavigating\"><i class=\"fa fa-spinner fa-spin fa-2x\"></i></li></ul></div></nav></template>"; });
+define('text!latay/latay.css', ['module'], function(module) { module.exports = ".latayPage {\n  position: relative;\n  display: inline-flex;\n  flex-wrap: wrap;\n  justify-content: space-around;\n  width: 100%;\n  height: 100%;\n  overflow: hidden;\n  top: 25px; }\n\n.flipContainer {\n  perspective: 1000px;\n  position: relative;\n  height: 200px;\n  width: 16%; }\n\n/* flip speed goes here */\n.flipper {\n  transition: 0.6s;\n  transform-style: preserve-3d;\n  position: relative;\n  text-align: center;\n  font-size: x-large;\n  background-color: darkkhaki;\n  height: 100%;\n  display: flex;\n  justify-content: center; }\n\n/* hide back of pane during swap */\n.frontCard, .backCard {\n  backface-visibility: hidden;\n  position: absolute;\n  top: 25%; }\n\n/* front pane, placed above back */\n.frontCard {\n  z-index: 2;\n  /* for firefox 31 */\n  transform: rotateY(0deg); }\n\n/* back, initially hidden pane */\n.backCard {\n  transform: rotateY(180deg); }\n\n.flipContainer.flip .flipper {\n  transform: rotateY(180deg); }\n"; });
 //# sourceMappingURL=app-bundle.js.map
--- src/latay/latay.ts
+++ src/latay/latay.ts
@@ -1,3 +1,38 @@
 export class Latay {
-  message = 'Se ya Latay!';
-}
+  private cards:any;
+
+  constructor () {
+    this.cards = [
+      {
+        front : "A",
+        back : "Makes any word an infinitive verb",
+        flip : false
+      },
+      {
+        front : "E",
+        back : "Mark of a direct object, accusative case",
+        flip : false
+      },
+      {
+        front : "I",
+        back : "And",
+        flip : false
+      },   
+      {
+        front : "O",
+        back : "Honorification, high style, politeness.'Omaw - please'.",
+        flip : false
+      },
+      {
+        front : "U",
+        back : "Some",
+        flip : false
+      }
+    ]
+  }
+
+  private flipCard(_card) {
+    _card.flip = !_card.flip;
+  }
+
+} // End of class Latay
